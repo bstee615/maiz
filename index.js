a0eda2a77c848acde3e4d1b3bdb0a8526546194b
bstee615 <benjaminjsteenhoek@gmail.com>
2020-07-10 17:52:33 -0500

    Split client into two pages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const WebSocket = require("ws");
+const uuid = require("uuid");
 const port = 3000;
 
 const app = express();
@@ -10,26 +11,43 @@ app.use(bodyParser.json());
 
 app.get("/", (_, res) => res.sendFile(path.join(__dirname + "/index.html")));
 
-const wsServer = new WebSocket.Server({
-  server: app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
-  ),
+app.get("/play", (req, res) => {
+  const username = req.params.username;
+  res.cookie("username", username);
+  res.sendFile(path.join(__dirname + "/play.html"));
 });
+
+const wsServer = new WebSocket.Server(
+  {
+    server: app.listen(port, () =>
+      console.log(`Server listening at http://localhost:${port}`)
+    ),
+    path: "/play",
+  },
+  () => console.log(`WS listening at http://localhost:${port}/play`)
+);
 
 let clients = {};
 let players = {};
-let id = 0;
 wsServer.on("connection", (socket) => {
-  const thisId = id;
-  clients[thisId] = socket;
-  console.log(`clients[${thisId}]`);
-  socket.on("message", (msgJson) => onMessage(msgJson, thisId));
-  id++;
-  //   const res = {
-  //     type: "id",
-  //     id: thisId,
-  //   };
-  //   socket.send(JSON.stringify(res));
+  socket.id = uuid.v4();
+  clients[socket.id] = socket;
+  console.log(`registered client`, socket.id);
+  socket.on("message", (msgJson) => onMessage(msgJson, socket.id));
+});
+
+wsServer.on("close", (socket) => {
+  const toDelete = clients[socket.id];
+  console.log(toDelete.id, "disconnected");
+  wsServer.clients.forEach((client) => {
+    client.send(
+      JSON.stringify({
+        type: "delete",
+        username: toDelete.username,
+      })
+    );
+  });
+  delete clients[socket.id];
 });
 
 function onMessage(msgJson, id) {
@@ -38,6 +56,7 @@ function onMessage(msgJson, id) {
   switch (msg.cmd) {
     case "register":
       console.log("register", msg.username);
+      client.username = msg.username;
       players[msg.username] = {
         x: 0,
         y: 0,
@@ -50,9 +69,9 @@ function onMessage(msgJson, id) {
       );
       break;
     case "move":
-      const username = msg.username;
+      const username = client.username;
       const delta = msg.delta;
-      console.log("moving", username, delta);
+      console.log("moving", delta);
 
       players[username].x += delta.x;
       players[username].y += delta.y;
