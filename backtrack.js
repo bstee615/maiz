1c707597eddb4201b45afd747086164c2e403969
bstee615 <benjaminjsteenhoek@gmail.com>
2020-07-11 00:18:38 -0500

    Refactor backtracking maze generation

--- backtrack.js
+++ backtrack.js
@@ -1,127 +1,120 @@
-/*
-Given a current cell as a parameter,
-    Mark the current cell as visited
-    While the current cell has any unvisited neighbour cells
-        Choose one of the unvisited neighbours
-        Remove the wall between the current cell and the chosen cell
-        Invoke the routine recursively for a chosen cell
-*/
+const config = require("./config");
 
-const w = 6;
-const h = 10;
-let grid = Array(h)
-  .fill()
-  .map(() => Array(w).fill(false));
+const w = config.maze.w;
+const h = config.maze.h;
 
-function getUnvisitedNeighbors(grid, r, c) {
-  let n = [];
-
-  if (r - 1 >= 0 && grid[r - 1][c] !== true) {
-    n.push({
-      row: r - 1,
-      col: c,
-    });
-  }
-  if (r + 1 < h && grid[r + 1][c] !== true) {
-    n.push({
-      row: r + 1,
-      col: c,
-    });
-  }
-  if (c - 1 >= 0 && grid[r][c - 1] !== true) {
-    n.push({
-      row: r,
-      col: c - 1,
-    });
-  }
-  if (c + 1 < w && grid[r][c + 1] !== true) {
-    n.push({
-      row: r,
-      col: c + 1,
-    });
-  }
-
-  return n;
+function vector(row, col) {
+  return {
+    row,
+    col,
+  };
 }
 
-function getWalls(w, h) {
-  let walls = [];
+function randInt(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function initGrid(w, h, initializer) {
+  let grid = [];
   for (let r = 0; r < h; r++) {
-    walls.push([]);
+    grid.push([]);
     for (let c = 0; c < w; c++) {
-      walls[r].push([]);
-      if (c - 1 >= 0) {
-        walls[r][c].push({ row: r, col: c - 1 });
-      }
-      if (c + 1 < w) {
-        walls[r][c].push({ row: r, col: c + 1 });
-      }
-      if (r - 1 >= 0) {
-        walls[r][c].push({ row: r - 1, col: c });
-      }
-      if (r + 1 < h) {
-        walls[r][c].push({ row: r + 1, col: c });
-      }
+      grid[r].push(initializer());
+    }
+  }
+  return grid;
+}
+
+function inBounds(r, c) {
+  return r >= 0 && r < h && c >= 0 && c < w;
+}
+
+// Execute func for neighbors in a cross
+function forCross(r, c, func) {
+  func(r - 1, c);
+  func(r + 1, c);
+  func(r, c - 1);
+  func(r, c + 1);
+}
+
+function initWalls(w, h) {
+  let walls = initGrid(w, h, () => []);
+  for (let r = 0; r < h; r++) {
+    for (let c = 0; c < w; c++) {
+      forCross(r, c, (ar, ac) => {
+        if (inBounds(ar, ac)) {
+          walls[r][c].push(vector(ar, ac));
+        }
+      });
     }
   }
   return walls;
 }
-let walls = getWalls(w, h);
 
-const randInt = (limit) => Math.floor(Math.random() * limit);
-
-function fill(grid, r, c) {
-  grid[r][c] = true;
-  let n = getUnvisitedNeighbors(grid, r, c);
-  while (n.length > 0) {
-    const idx = randInt(n.length);
-    const o = n[idx];
-    const or = o.row;
-    const oc = o.col;
-    if (n.length === 1) {
-      n = [];
-    } else {
-      n = n.splice(idx, 1);
+function getUnvisitedNeighbors(grid, r, c) {
+  let n = [];
+  forCross(r, c, (ar, ac) => {
+    if (inBounds(ar, ac) && grid[ar][ac] !== true) {
+      n.push(vector(ar, ac));
     }
-    if (grid[or][oc] !== true) {
-      grid[or][oc] = true;
-      walls[r][c] = walls[r][c].filter(
-        ({ row, col }) => row !== or || col !== oc
-      );
-      walls[or][oc] = walls[or][oc].filter(
-        ({ row, col }) => row !== r || col !== c
-      );
-      fill(grid, or, oc);
+  });
+  return n;
+}
+
+// Remove b from a's walls
+function disconnectOneWay(walls, a, b) {
+  walls[a.row][a.col] = walls[a.row][a.col].filter(
+    ({ row, col }) => row !== b.row || col !== b.col
+  );
+}
+
+// DFS from row, col until we hit a dead end.
+function search(grid, walls, row, col) {
+  grid[row][col] = true;
+  let unvisited = getUnvisitedNeighbors(grid, row, col);
+  while (unvisited.length > 0) {
+    const index = randInt(unvisited.length);
+    const me = { row, col };
+    const other = unvisited[index];
+
+    unvisited.splice(index, 1);
+
+    if (grid[other.row][other.col] !== true) {
+      grid[other.row][other.col] = true;
+      disconnectOneWay(walls, me, other);
+      disconnectOneWay(walls, other, me);
+      search(grid, walls, other.row, other.col);
     }
   }
 }
 
-while (
-  grid
-    .map((r) => r.filter((i) => i === false).length)
-    .reduce((a, b) => a + b, 0) > 0
-) {
-  const unvisitedCells = grid
-    .map((r, ir) =>
-      r
-        .map((e, ic) => (e === false ? { row: ir, col: ic } : null))
-        .filter((e) => e != null)
-    )
-    .flat();
-  const choice = unvisitedCells[randInt(unvisitedCells.length)];
-  fill(grid, choice.row, choice.col);
+// Backtracking DFS repeatedly until all cells have been visited
+function generateMaze() {
+  let grid = initGrid(w, h, () => false);
+  let walls = initWalls(w, h);
+
+  const getUnvisitedCellsInGrid = () =>
+    grid
+      .map((r, ir) =>
+        r
+          .map((e, ic) => (e === false ? { row: ir, col: ic } : null))
+          .filter((e) => e != null)
+      )
+      .flat();
+  const gridHasUnvisitedCells = () => getUnvisitedCellsInGrid().length > 0;
+
+  while (gridHasUnvisitedCells()) {
+    const unvisited = getUnvisitedCellsInGrid();
+    const choice = unvisited[randInt(unvisited.length)];
+    search(grid, walls, choice.row, choice.col);
+  }
+
+  return walls;
 }
-console.log("done");
+
+const walls = generateMaze();
+
 console.log(walls);
-console.log(grid);
-
-// const squareSize = 100;
-// var fs = require("fs");
-// var { createCanvas } = require("canvas"),
-//   canvas = createCanvas(w * squareSize, h * squareSize),
-//   ctx = canvas.getContext("2d");
-
-// console.log("canvas", w * squareSize, h * squareSize);
 
 sock = require("./socket");
 sock.send(walls);
