f218cf269f2cf9baf3780b5b33c043ab032807e3
bstee615 <benjaminjsteenhoek@gmail.com>
2020-07-14 16:27:07 -0500

    Fix double dipping map gen forwarding

--- mazedraw.js
+++ mazedraw.js
@@ -3,15 +3,16 @@
 const net = require("net");
 const config = require("./config");
 
-let client = net.Socket();
-
 exports.draw = function (width, height, maze, cb) {
   const { walls, startingPoint, ends } = maze;
+
+  let client = net.Socket();
   client.connect(config.maze.port, "localhost");
   client.write(JSON.stringify({ width, height, walls, startingPoint, ends }));
 
   client.on("data", function (data) {
     cb(data);
+    client.destroy();
   });
 
   client.end();
