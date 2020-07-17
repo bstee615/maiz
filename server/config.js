d145b35cd7c531ef849c3d6068c7b5c4d4fa46cd
bstee615 <benjaminjsteenhoek@gmail.com>
2020-07-17 17:06:11 -0500

    Add MAZEDRAW_URL

--- server/config.js
+++ server/config.js
@@ -1,5 +1,8 @@
 exports.port = process.env.NODE_ENV === "production" ? 80 : 3000;
 exports.host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
+exports.mazedraw = {
+  host: process.env.MAZEDRAW_URL || "localhost",
+};
 
 const fs = require("fs");
 const path = require("path");
--- server/mazedraw.js
+++ server/mazedraw.js
@@ -7,7 +7,10 @@ exports.draw = function (width, height, maze, cb) {
   const { walls, startingPoint, ends } = maze;
 
   let client = net.Socket();
-  client.connect(config.maze.port, "localhost");
+  client.connect(config.maze.port, config.mazedraw.host);
+  console.log(
+    `Sending maze request to ${config.mazedraw.host}:${config.maze.port}`
+  );
   client.write(JSON.stringify({ width, height, walls, startingPoint, ends }));
 
   client.on("data", function (data) {
